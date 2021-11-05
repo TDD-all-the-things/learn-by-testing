a81ebaec5ea036b60b1de3bb5e9cd29cdfcc76bd
longyue0521 <longyueli0521@gmail.com>
2021-11-05 12:39:25 +0800

    test(Viper): add test to read from etcd/docker-compose.yaml

--- viper/viper_test.go
+++ viper/viper_test.go
@@ -1,4 +1,4 @@
-package viper_test
+package main_test
 
 import (
 	"testing"
@@ -14,11 +14,21 @@ func TestViper(t *testing.T) {
 	viper.AddConfigPath(".")              // optionally look for config in the working directory
 	err := viper.ReadInConfig()           // Find and read the config file
 	assert.NoError(t, err)
-	
+
 	assert.Equal(t, "TOML Example", viper.GetString("title"))
 	assert.Equal(t, "192.168.1.1", viper.GetString("database.server"))
 	assert.Equal(t, []int{8000, 8001, 8002}, viper.GetIntSlice("database.ports"))
 
 	assert.Equal(t, "10.0.0.1", viper.GetString("servers.alpha.ip"))
 	assert.Equal(t, []interface{}{[]interface{}{"gamma", "delta"}, []interface{}{int64(1), int64(2)}}, viper.Get("clients.data"))
+}
+
+func TestYAML(t *testing.T) {
+	viper.SetConfigName("docker-compose")
+	viper.AddConfigPath("../etcd/")
+
+	err := viper.ReadInConfig()
+	assert.NoError(t, err)
+
+	assert.Equal(t, []string{"12379:2379", "12380:2380"}, viper.GetStringSlice("services.etcd1.ports"))
 }
